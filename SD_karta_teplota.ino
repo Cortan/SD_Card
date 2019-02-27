98010aee0047a748c101a6929dde3b712aa47f07
Cortan <47772664+Cortan@users.noreply.github.com>
2019-02-27 10:32:24 +0100

    Update SD_karta_teplota.ino

--- SD_karta_teplota.ino
+++ SD_karta_teplota.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define RT0 10000   // Odpor termistora
-#define B 3977      // Beta faktor 
-#define VCC 3.30    // Napájanie U= 3.3V
-#define R 10000    // Odpor použitého rezistora R=10KΩ
+#define RT0 10000   //Resistance of resistor
+#define B 3977      //Beta factor 
+#define VCC 3.30    //Power supply U= 3.3V
+#define R 10000     //Resistance of the resistor used: R=10 KΩ
 
 float RT, VR, ln, TX, T0, VRT, teplota;
 const int chipSelect = 53;
@@ -14,18 +14,19 @@ void setup()
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ;                 
+  //wait for serial port to connect
   }
 
   T0 = 30 + 273.15;
-  Serial.print("Inicializacia SD karty..");
+  Serial.print("Initialization SD card...");
 
   if (!SD.begin(chipSelect)) {
-    Serial.println("Nacitavanie zlyhalo.");
-    // don't do anything more:
+    Serial.println("Loading failed!");
+      // don't do anything more:
     return;
   }
-  Serial.println("Katra nacitana.");
+  Serial.println("Card loaded.");
 }
 
 void loop()
@@ -34,21 +35,21 @@ void loop()
   for (int analogPin = 0; analogPin < 1; analogPin++) {
      VRT = analogRead(analogPin);
 
-  VRT = (3.30 / 1023.00) * VRT;      // Prepočet na volty
+  VRT = (3.30 / 1023.00) * VRT;      //Voltage conversion
   VR = VCC - VRT;
-  RT = VRT / (VR / R);               // Odpor RT
+  RT = VRT / (VR / R);               //Resistance RT
 
   ln = log(RT / RT0);
-  TX = (1 / ((ln / B) + (1 / T0)));  // Teplota z termistora v Kelvinoch
+  TX = (1 / ((ln / B) + (1 / T0)));  //Temperature of termistor (K)
 
-  teplota = (TX - 273.15);                  // Prepočet z Kelvinov na °C
+  teplota = (TX - 273.15);                  //Temperature conversion to °C
     
     dataString += String(teplota);
     if (analogPin > 1) {
       dataString += ",";
     }
   }
-  File dataFile = SD.open("telota.txt", FILE_WRITE);
+  File dataFile = SD.open("Temperature.txt", FILE_WRITE);
 
   if (dataFile) {
     dataFile.println(dataString);
@@ -57,7 +58,7 @@ void loop()
     delay(500);
   }
   else {
-    Serial.println("problem s otvenim telota.txt");
+    Serial.println("Problem with opening Temperature.txt");
     delay(10000);
   }
 }
